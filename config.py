7ba73153c4884a6e6842fab9d31945c1b6e198d2
Dev_ <153924592+RohitS5612@users.noreply.github.com>
2025-08-19 12:18:10 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+IRCSERVER = "irc.esper.net"
+IRCPORT = 6667
+IDENT = "irc.devm.rf.gd"
+NICK = "Test"  # Change to whatever nick you want
+CHANNELS = ["#minecraft"]  # Optional default channels
